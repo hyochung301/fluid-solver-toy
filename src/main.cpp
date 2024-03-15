dc581f3b4deeff31cd4153fa70167a6d0248cec8
collebrusco <collebrusco.frank@gmail.com>
2024-03-15 05:10:49 -0500

    ughhhhh

--- src/main.cpp
+++ src/main.cpp
@@ -56,108 +56,54 @@ void log_sum(int n, float* u, float* v) {
 */
 void stable_solve(int n, float* u, float* v, float* u0, float* v0, float const& visc, float const& dt)
 {
-	float x, y, x0, y0, f, r, U[2], V[2], s, t;
-	int i, j, i0, j0, i1, j1;
+	float x, y, f, r, U[2], V[2], s, t;
+    int i, j, i0, j0, i1, j1;
 
-	ls("force", n, u0, v0);
+    for ( i=0 ; i<n*n ; i++ ) {
+        u[i] += dt*u0[i]; u0[i] = u[i];
+        v[i] += dt*v0[i]; v0[i] = v[i];
+    }
 
-	// add force fields to velo fields
-	// then make force fields equal velo field
-	for (i = 0; i < n*n; i++) {
-		u[i] += dt*u0[i]; u0[i] = u[i];
-		v[i] += dt*v0[i]; v0[i] = v[i];
-	}
+    for ( i=0 ; i<n ; i++ ) {
+        for ( j=0 ; j<n ; j++ ) {
+            x = i-dt*u0[i+n*j]*n; y = j-dt*v0[i+n*j]*n;
+            i0 = floor(x); s = x-i0; i0 = (n+(i0%n))%n; i1 = (i0+1)%n;
+            j0 = floor(y); t = y-j0; j0 = (n+(j0%n))%n; j1 = (j0+1)%n;
+            u[i+n*j] = (1-s)*((1-t)*u0[i0+n*j0]+t*u0[i0+n*j1])+
+                          s *((1-t)*u0[i1+n*j0]+t*u0[i1+n*j1]);
+            v[i+n*j] = (1-s)*((1-t)*v0[i0+n*j0]+t*v0[i0+n*j1])+
+                          s *((1-t)*v0[i1+n*j0]+t*v0[i1+n*j1]);
+        }
+    }
 
-	ls("after force", n, u, v);
+    for ( i=0 ; i<n ; i++ )
+        for ( j=0 ; j<n ; j++ )
+            { u0[i+(n+2)*j] = u[i+n*j]; v0[i+(n+2)*j] = v[i+n*j]; }
 
-	/*
-		... "This effect is called “self-advection”: an auto-feedback mechanism
-		within the fluid. To solve for this effect we use a very elegant technique 
-		known as a “semi-Lagrangian” scheme in the computational fluid dynamics literature. 
-		Here is how it works. For each voxel of the velocity grid we trace its midpoint backwards 
-		through the velocity field over a time step dt. This point will end up somewhere else 
-		in the grid. We then linearly interpolate the velocity at that point from the 
-		neighboring voxels and transfer this velocity back to the departure voxel. 
-		This requires two grids, one for the interpolation (u0,v0) and one to store 
-		the new interpolated values (u,v). To perform the particle trace, we use a 
-		simple linear approximation of the exact path. 
-		The interpolation is very easy to implement because our grids are periodic. 
-		Points that leave the grid simply reenter the grid from the opposite side."
-	*/
-	for (x=0.5/n, i=0; i < n; i++, x+=1.0/n) {
-		for (y=0.5/n, j=0; j < n; j++, y+=1.0/n) {
-			x0 = n*(x-dt*u0[i+n*j]) - 0.5;
-			y0 = n*(y-dt*v0[i+n*j]) - 0.5;
-			i0 = floor(x0); s = x0-i0; i0 = (n+(i0%n))%n; i1 = (i0+1)%n;
-			j0 = floor(y0); t = y0-j0; j0 = (n+(j0%n))%n; j1 = (j0+1)%n;
-			u[i+n*j] = ((1-s) * ( ((1-t) * u0[i0+n*j0]) + (t*u0[i0+n*j1]) )) + 
-						  (s  * ( ((1-t) * u0[i1+n*j0]) + (t*u0[i1+n*j1]) ));
-			v[i+n*j] = ((1-s) * ( ((1-t) * v0[i0+n*j0]) + (t*v0[i0+n*j1]) )) + 
-						  (s  * ( ((1-t) * v0[i1+n*j0]) + (t*v0[i1+n*j1]) ));
-			// if ((i == 6) || u[i+n*j] != 0.0 || v[i+n*j] != 0.0) 
-			// 	LOG_DBG("%f", t);
-			// u[i+n*j] = u0[i+n*j];
-			// v[i+n*j] = v0[i+n*j];
-		}
-	}
+    fftwf_execute(forward_u); fftwf_execute(forward_v);
 
-	ls("after self advec", n, u, v);
+    for ( i=0 ; i<=n ; i+=2 ) {
+        x = 0.5*i;
+        for ( j=0 ; j<n ; j++ ) {
+            y = j<=n/2 ? j : j-n;
+            r = x*x+y*y;
+            if ( r==0.0 ) continue;
+            f = exp(-r*dt*visc);
+            U[0] = u0[i  +(n+2)*j]; V[0] = v0[i  +(n+2)*j];
+            U[1] = u0[i+1+(n+2)*j]; V[1] = v0[i+1+(n+2)*j];
+            u0[i  +(n+2)*j] = f*( (1-x*x/r)*U[0]     -x*y/r *V[0] );
+            u0[i+1+(n+2)*j] = f*( (1-x*x/r)*U[1]     -x*y/r *V[1] );
+            v0[i+  (n+2)*j] = f*(   -y*x/r *U[0] + (1-y*y/r)*V[0] );
+            v0[i+1+(n+2)*j] = f*(   -y*x/r *U[1] + (1-y*y/r)*V[1] );
+        }
+    }
 
-	// copy interpolated values back to fft buffers
-	for (i = 0; i < n; i++) {
-		for (j = 0; j < n; j++) {
-			u0[i+(n+2)*j] = u[i+n*j];
-			v0[i+(n+2)*j] = v[i+n*j];
-		}
-	}
+    fftwf_execute(inv_u); fftwf_execute(inv_v);
 
-	ls("0s pre fft", n, u0, v0);
-
-	fftwf_execute(forward_u); fftwf_execute(forward_v); // transform velo field
-
-	ls("0s post fft", n, u0, v0);
-
-	// apply visc filter and make mass conserving. 
-	// can make mask conserving by projecting fourier velo field
-	// onto line perpendicular to wave num
-	for (i = 0; i <= n; i += 2) {
-		x = 0.5*i;
-		for (j = 0; j < n; j++) {
-			// calculate x,y,r relative to origin
-			y = j<=n/2 ? j : j-n;
-			r = x*x+y*y;
-			if (r == 0.0) continue;
-			f = exp(-r*dt*visc);	// this filter converges to true fluid eqs
-			// U[0,1] is R,I of x coord, V[0,1] is R,I of y coord
-			U[0] = u0[i  +(n+2)*j]; V[0] = v0[i  +(n+2)*j];
-			U[1] = u0[i+1+(n+2)*j]; V[1] = v0[i+1+(n+2)*j];
-			// project and apply filter
-			u0[i  +(n+2)*j] = f*( (1-x*x/r)*U[0]      -x*y/r *V[0] );
-			u0[i+1+(n+2)*j] = f*( (1-x*x/r)*U[1]      -x*y/r *V[1] );
-			v0[i  +(n+2)*j] = f*(    -y*x/r*U[0]  + (1-y*y/r)*V[0] );
-			v0[i+1+(n+2)*j] = f*(    -y*x/r*U[1]  + (1-y*y/r)*V[1] );
-		}
-	}
-
-	ls("0s post filter", n, u0, v0);
-
-	fftwf_execute(inv_u); fftwf_execute(inv_v); // inverse transform
-
-	ls("0s post inverse", n, u0, v0);
-
-	// normalize 
-	f = 1.0/(n*n);
-	for (i = 0; i < n; i++) {
-		for (j = 0; j < n; j++) {
-			u[i+n*j] = f*u0[i+(n+2)*j];
-			v[i+n*j] = f*v0[i+(n+2)*j];
-		}
-	}
-
-	memset(u0, 0x00, n*(n+2)*sizeof(float));
-	memset(v0, 0x00, n*(n+2)*sizeof(float));
-
-	ls("returning", n, u, v);
+    f = 1.0/(n*n);
+    for ( i=0 ; i<n ; i++ )
+        for ( j=0 ; j<n ; j++ )
+            { u[i+n*j] = f*u0[i+(n+2)*j]; v[i+n*j] = f*v0[i+(n+2)*j]; }
 }
 
 struct Field {
